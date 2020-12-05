68ed90a695b7d5f7b9e07223431f522175a32a13
Sanuja Tharinda <sanujatharinda@gmail.com>
2020-12-06 02:33:02 +0530

    Project Initial Cleaned Up Template
    Initial Front End Cleanup Template
    
    Project initial template after cleanup
    
    Project initial cleanup template complete
    
    Project initial template clean up

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return ( 
+      <>
+        <h1>Welcome To Single Vendor E-Commerce Platform</h1>
+      </>
+   );
+}
+ 
+export default App;
+
