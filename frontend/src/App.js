514a8448d0171f6ee4a4ff06f9a50132eb58ad03
vishvajith17 <maduka.18@cse.mrt.ac.lk>
2021-02-08 17:40:28 +0530

    shipping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
 import Cart from './screens/Cart';
 import LoginForm from './screens/LoginForm';
+import ShippingScreen from './screens/ShippingScreen';
 import Footer from './components/Footer';
+
+
 
 const App = () => {
   return ( 
@@ -14,9 +18,14 @@ const App = () => {
         <Container >
           <main>
             <Switch>
+              <Route path='/products/:id' component={ProductScreen} />
+              <Route path='/shipping' component={ShippingScreen} />
               <Route path='/login' component={LoginForm} />
               <Route path='/cart' component={Cart} />
-              <Route path='/' component={HomeScreen} />
+              
+              <Route path='/' exact component={HomeScreen} />
+             
+               
             </Switch>
           </main>
         </Container>
