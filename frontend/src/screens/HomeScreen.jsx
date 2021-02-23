68192c4beeb4290826821816450f2446e0270fde
Sanuja Tharinda <sanujatharinda@gmail.com>
2021-02-23 23:38:55 +0530

    Finish Home Screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,8 +4,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import ButtonSpinner from './../components/common/ButttonSpinner';
 import CommonListGroup from './../components/common/CommonListGroup';
+import Pagination from '../components/common/Pagination';
 import { loadProducts, getAllProducts, getProductLoadingStatus } from './../store/entities/products';
 import { loadCategories, getCategoriesLoadingStatus, getAllCategories } from './../store/entities/categories';
+import { paginate } from './../utils/paginate';
+
 
 
 const HomeScreen = ({ match }) => {
@@ -14,16 +17,23 @@ const HomeScreen = ({ match }) => {
     const productsLoading = useSelector(getProductLoadingStatus);
     const categories = useSelector(getAllCategories);
     const categoriesLoading = useSelector(getCategoriesLoadingStatus);
-    const [filtered, setFiltered] = useState([]);
+
     const [selectedCategory, setSelectedCategory] = useState('All');
+    const [filtered, setFiltered] = useState(products);
+
+    const pageSize = 3;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [paginated, setPaginated] = useState([]);
   
     useEffect(() => {
         dispatch(loadProducts());
         dispatch(loadCategories());
-        setFiltered(getFilteredProducts(products, categories, selectedCategory));
+        const updatedFiltered = getFilteredProducts(products, categories, selectedCategory);
+        setFiltered(updatedFiltered);
+        setPaginated(paginate(updatedFiltered, currentPage, pageSize));
         console.log("Loading Products", products);
         console.log("Loading Categories", categories);
-    }, [products, categories, dispatch, selectedCategory]);
+    }, [products, categories, dispatch]);
 
     return (
         <div className = 'mb-4'>
@@ -31,16 +41,31 @@ const HomeScreen = ({ match }) => {
             {productsLoading && <ButtonSpinner message = "Loading Products..." />}
             {!categoriesLoading &&
                 <CommonListGroup 
-                    onSelect = {(category) => {setSelectedCategory(category);}} 
+                    onSelect = {(category) => {
+                        setSelectedCategory(category);
+                        const updatedFiltered = getFilteredProducts(products, categories, category);
+                        setFiltered(updatedFiltered);
+                        setPaginated(paginate(updatedFiltered, 1, pageSize));
+                    }} 
                     defaultSelected = {"All"} selected={selectedCategory} 
                     list={categories.map(c => c.category.name)}
                 />}
             <Row>
-                {filtered.map( product => 
+                {paginated.map( product => 
                 <Col key = {product.productId + product.variantName} sm = {1} md = {2} lg = {3} xl = {4}>
                     <Product product= {product}></Product>
                 </Col>)} 
             </Row>
+
+            <Pagination
+                itemsCount = {filtered.length} 
+                pageSize = {pageSize} 
+                currentPage = {currentPage}
+                onPageChange = {(page) => {
+                    setCurrentPage(page);
+                    setPaginated(paginate(filtered, page, pageSize));
+                }}
+            />
         </div>
     );
 }
@@ -52,5 +77,7 @@ function getFilteredProducts(products, categories, filter){
     const selectedCategoryId = category.category.categoryId;
     return products.filter(p => p.categories.includes(selectedCategoryId));
 }
+
+
 export default HomeScreen;
 
