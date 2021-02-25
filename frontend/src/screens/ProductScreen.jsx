bc51dde214fe8a21e3909567098b78f48cf48bd2
Shamila Nuwan <shamila.18@cse.mrt.ac.lk>
2021-02-25 16:49:56 +0530

    product screen fixes

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,25 +1,116 @@
-import React, { useEffect } from 'react'
-// import { Link } from 'react-router-dom'
-import { getProductById, loadProducts } from '../store/entities/products';
+import React, { useEffect ,useState} from 'react'
+import { Link } from 'react-router-dom'
+import { getProductById, loadProducts, productUpdated } from '../store/entities/products';
 import { useDispatch, useSelector } from 'react-redux';
-// import { Button } from 'react-bootstrap';
+import { Button, Row,Col,Image ,ListGroupItem,ListGroup,Card,Table} from 'react-bootstrap';
+import Rating from '../components/Rating'
 // import { addToCart, removeFromCart, emptyCart, updateItemCount } from './../store/cart';
 // import { invokeToast } from '../store/toastAction';
 
 const ProductScreen = ({match}) => {
     const dispatch = useDispatch();
     const product = useSelector(getProductById(match.params.productId));
+    
+    const [rating, setRating ] = useState(0);
 
     useEffect(() => {
-        dispatch(loadProducts())
-    });
+        dispatch(loadProducts());
+        setRating(calculateRating(product.reviews));
+        
+        console.log(product);
+        
+    }, [product]);
+
+    
 
     return (
         <>
-            <div>{product.title}</div>
-            /**
-                Just Examples
-             */
+            
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+
+            <Row>
+                <Col md={6}>
+                    <Image src={`/images/${product.productId}.jpg`} fluid/>
+                </Col>
+                <Col md={6}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>{product.title}</h2>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating rating={rating} message = {rating === 0 ? "This Product has not been rated yet" : `${rating} From ${product.reviews.length} Reviews`}></Rating>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h5>{product.description}</h5>
+                        </ListGroup.Item>
+                        
+                    </ListGroup>
+                    <ListGroup variant="flush">
+                        <h5>Reviews :</h5>
+                            {product.reviews.map(r=><ListGroup.Item>
+                                
+                                <Rating rating={r.rating}></Rating>
+                                <h6>{r.description}</h6>
+                            </ListGroup.Item>)}
+                    </ListGroup>
+                </Col>
+                
+            </Row>
+            <h5>Variants :</h5>
+            {/* <Row >
+                
+                <Col md={6}>
+                    <ListGroupItem> name </ListGroupItem>
+                            {product.variants.map(v=><ListGroup.Item>
+                                <h6>{v.name}</h6>
+                            </ListGroup.Item>)}  
+                </Col>
+                <Col md={4}>
+                    <ListGroupItem> unit price </ListGroupItem>
+                            {product.variants.map(v=><ListGroup.Item>
+                                <h6>{v.unitPrice}</h6>
+                            </ListGroup.Item>)}
+                </Col>
+
+                <Col md={2}>
+                    <ListGroupItem> count in stock </ListGroupItem>
+                            {product.variants.map(v=><ListGroup.Item>
+                                <h6>{v.countInStock}</h6>
+                            </ListGroup.Item>)}
+                </Col>
+                   
+            
+            </Row> */}
+
+            <Table >
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>unit price</th>
+                        <th>count in stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <td>
+                            {product.variants.map(v=><ListGroup.Item>
+                                <h6>{v.name}</h6>
+                            </ListGroup.Item>)}
+                    </td>
+                    <td>
+                            {product.variants.map(v=><ListGroup.Item>
+                                <h6>{v.unitPrice}</h6>
+                            </ListGroup.Item>)}
+                    </td>
+                    <td>
+                            {product.variants.map(v=><ListGroup.Item>
+                                <h6>{v.countInStock}</h6>
+                            </ListGroup.Item>)}
+                    </td>
+                </tbody>
+            </Table>
+            
             {/* <Button onClick ={ () => dispatch(addToCart(product))}>
                 Add To Cart
             </Button>
@@ -41,3 +132,13 @@ const ProductScreen = ({match}) => {
 }
 
 export default ProductScreen;
+
+function calculateRating(reviews){
+    if(reviews.length === 0) return 0;
+    let rating = 0;
+    for(let review of reviews){
+        rating += parseFloat(review.rating);
+    }
+    rating = rating /reviews.length;
+    return rating.toFixed(1);
+}
